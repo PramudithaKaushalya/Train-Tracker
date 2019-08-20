60e6b5e15e84a692cc9ad38a7a8c73cb2ccefe36
Pramuditha Kaushalya <pramuditha.kaushalya@gmail.com>
2019-08-20 18:22:36 +0530

    hosted

---
+++ dist/precache-manifest.3f06aa894757afb709940d68a854232f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb4d5021a2c4b87655fa8de28bd20bbc",
+    "url": "/static/media/Logo.fb4d5021.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7630c00682a399d1c1e0",
+    "url": "/static/js/main.936d85b7.chunk.js"
+  },
+  {
+    "revision": "5ccc51e64d63e63e640a",
+    "url": "/static/js/2.18461249.chunk.js"
+  },
+  {
+    "revision": "7630c00682a399d1c1e0",
+    "url": "/static/css/main.8c2831f5.chunk.css"
+  },
+  {
+    "revision": "49cb56ec0a2ff2dc2be3bc6836dc5065",
+    "url": "/index.html"
+  }
+];
--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -15,15 +15,3 @@ var config = {
   //firebase.firestore().settings({ timestampsInSnapshots: true});
 
   export default fbConfig;
-
- 
-  // firebase.initializeApp(config);
-
-  // var config = {
-  //   apiKey: "AIzaSyAW1bd_wUKCTVXsWoi_Wn39c1HaNX-aX1c",
-  //   authDomain: "train-tracker-232105.firebaseapp.com",
-  //   databaseURL: "https://train-tracker-232105.firebaseio.com",
-  //   projectId: "train-tracker-232105",
-  //   storageBucket: "train-tracker-232105.appspot.com",
-  //   messagingSenderId: "939799566101"
-  // };
